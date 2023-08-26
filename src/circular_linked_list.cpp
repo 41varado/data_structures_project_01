a77ac5fceab237ee9b0361c511f2c810af6e9d9b
Len <104241467+Florenzio@users.noreply.github.com>
2023-08-26 02:17:53 -0600

    Update circular_linked_list.cpp
    Add find and remove functions for project management
    
    Implement the find function to search for a project in the circular linked list.
    The remove function enables the deletion of a project from the list while maintaining list integrity.

--- src/circular_linked_list.cpp
+++ src/circular_linked_list.cpp
@@ -8,12 +8,58 @@ using namespace std;
 
 bool circular_linked_list::remove(const NODE& node)
 {
+if (head_node == nullptr) {
+        return false;  
+    }
 
+    NODE* current_node = head_node;
+    NODE* previous_node = nullptr;
+
+    do {
+        if (current_node->data == node) {
+            if (current_node == head_node) {
+                if (current_node->next_node == head_node) {
+                   
+                    delete current_node;
+                    head_node = tail_node = nullptr;
+                } else {
+                    head_node = current_node->next_node;
+                    tail_node->next_node = head_node;
+                    delete current_node;
+                }
+            } else if (current_node == tail_node) {
+                tail_node = previous_node;
+                previous_node->next_node = head_node;
+                delete current_node;
+            } else {
+                previous_node->next_node = current_node->next_node;
+                delete current_node;
+            }
+            return true;  
+        }
+
+        previous_node = current_node;
+        current_node = current_node->next_node;
+    } while (current_node != head_node);
+
+    return false; 
 }
 
 NODE* circular_linked_list::find(const PROJECT& project)
 {
+NODE* current_node = head_node;
+    
+    if (head_node) {
+        do {
+            if (current_node->data == project) {
+                
+                return current_node;
+            }
+            current_node = current_node->next_node;
+        } while (current_node != head_node);
+    }
 
+    return nullptr;
 }
 
 void circular_linked_list::append(const PROJECT& project)
